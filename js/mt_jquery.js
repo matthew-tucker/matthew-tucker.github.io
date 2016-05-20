ea3e99f183184d988ebcae0b3b687bb8192f85bb
Matt Tucker <matt.tucker@nyu.edu>
2016-05-20 17:00:01 +0400

    navbar sticky start, contact info

--- js/mt_jquery.js
+++ js/mt_jquery.js
@@ -47,3 +47,15 @@
     }
   });
 })(jQuery);
+
+$(document).ready(function() {
+    $(document).scroll(function () {
+        var scroll = $(this).scrollTop();
+        var topDist = $(".img-banner").position();
+        if (scroll > topDist.top) {
+            $('.site-nav').addClass("nav-fixed");
+        } else {
+            $('.site-nav').removeClass("nav-fixed");
+        }
+    });
+});
